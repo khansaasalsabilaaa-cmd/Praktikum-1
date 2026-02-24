1b9b9b8647442e913c70b2ce3d0b626aa4952b7d
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-02-24 19:34:00 +0700

    menambahkan deklarasi variabel luas

--- file1.cpp
+++ file1.cpp
@@ -12,4 +12,19 @@ int main(){
 
 int main(){
     int p, l, luas;
-}
+    
+    //langkah 2
+    cout << "Masukkan panjang = ";
+    cin >> p;
+
+    //langkah 3
+    cout << "masukkan lebar = ";
+    cin >> l;
+
+    //langkah 4
+    luas = p * l;
+
+    //langkah 5
+    cout << "Hasilnya = " << luas << endl;
+    cout << "terimakasih";
+} //selesai
